fb49357a75393cb70d7e7523e024011a05efa574
Luga Wang <luga.wang@unity3d.com>
2023-09-18 14:22:02 -0700

    add new files

---
+++ iOS/Objective C/IronSourceDemoApp/NativeAdViewController.h
@@ -0,0 +1,13 @@
+//
+//  NativeAdViewController.h
+//  IronSourceDemoApp
+//
+//  Created by Luga Wang on 9/15/23.
+//  Copyright © 2023 Unity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NativeAdViewController : UIViewController
+
+@end
---
+++ iOS/Objective C/IronSourceDemoApp/NativeAdViewController.m
@@ -0,0 +1,15 @@
+//
+//  NativeAdViewController.m
+//  IronSourceDemoApp
+//
+//  Created by Luga Wang on 9/15/23.
+//  Copyright © 2023 Unity. All rights reserved.
+//
+
+#import "NativeAdViewController.h"
+#import <IronSource/IronSource.h>
+
+@implementation NativeAdViewController () <LevelPlayNativeAdDelegate>
+
+
+@end
