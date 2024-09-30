4730c5e9a69112054c8be6274442bd6b1ac11629
guylis <guy.lis@is.com>
2024-09-30 17:20:17 +0300

    replace legacy ad units banner and interstitial with multiple ad unit API for android java demo

--- Android/Java/app/src/main/java/com/ironsource/ironsourcesdkdemo/DemoActivity.java
+++ Android/Java/app/src/main/java/com/ironsource/ironsourcesdkdemo/DemoActivity.java
@@ -12,18 +12,26 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 import androidx.multidex.BuildConfig;
-import com.ironsource.mediationsdk.ISBannerSize;
 import com.ironsource.mediationsdk.IronSource;
-import com.ironsource.mediationsdk.IronSourceBannerLayout;
 import com.ironsource.mediationsdk.integration.IntegrationHelper;
 import com.ironsource.mediationsdk.model.Placement;
 import com.ironsource.mediationsdk.utils.IronSourceUtils;
+import com.unity3d.mediation.LevelPlay;
+import com.unity3d.mediation.LevelPlayAdSize;
+import com.unity3d.mediation.LevelPlayInitRequest;
+import com.unity3d.mediation.banner.LevelPlayBannerAdView;
+import com.unity3d.mediation.interstitial.LevelPlayInterstitialAd;
+
 import java.util.Arrays;
+import java.util.List;
 
 public class DemoActivity extends Activity implements DemoActivityListener {
 
     public static final String TAG = "DemoActivity";
     public static final String APP_KEY = "85460dcd";
+
+    public static final String INTERSTITIAL_AD_UNIT_ID = "aeyqi3vqlv6o8sh9";
+    public static final String BANNER_AD_UNIT_ID = "thnfvcsog13bhn08";
 
     private Button rewardedVideoShowButton;
     private Button interstitialLoadButton;
@@ -32,8 +40,10 @@ public class DemoActivity extends Activity implements DemoActivityListener {
     private Button bannerDestroyButton;
 
     private FrameLayout bannerParentLayout;
-    private IronSourceBannerLayout ironSourceBannerLayout;
     private Placement rewardedVideoPlacementInfo;
+
+    LevelPlayInterstitialAd mInterstitialAd;
+    LevelPlayBannerAdView mBannerAd;
 
     //region Lifecycle Methods
     @Override
@@ -81,23 +91,28 @@ public class DemoActivity extends Activity implements DemoActivityListener {
             IntegrationHelper.validateIntegration(this);
         }
 
-        // Before initializing any of our products (Rewarded video, Interstitial or Banner) you must set
+        // Before initializing any of our legacy products (Rewarded video, Interstitial or Banner) you must set
         // their listeners. Take a look at each of these listeners method and you will see that they each implement a product
         // protocol. This is our way of letting you know what's going on, and if you don't set the listeners
         // we will not be able to communicate with you.
         // We're passing 'this' to our listeners because we want
         // to be able to enable/disable buttons to match ad availability.
         IronSource.setLevelPlayRewardedVideoListener(new DemoRewardedVideoAdListener(this));
-        IronSource.setLevelPlayInterstitialListener(new DemoInterstitialAdListener(this));
         IronSource.addImpressionDataListener(new DemoImpressionDataListener());
 
         // After setting the listeners you can go ahead and initialize the SDK.
+
+        // Init the SDK when implementing the Multiple Ad Units Interstitial and Banner API, and Rewarded using legacy APIs
+        List<LevelPlay.AdFormat> legacyAdFormats = Arrays.asList(LevelPlay.AdFormat.REWARDED);
+
+
+        LevelPlayInitRequest initRequest = new LevelPlayInitRequest.Builder(APP_KEY)
+                .withLegacyAdFormats(legacyAdFormats)
+                .build();
+        LevelPlay.init(this, initRequest, new DemoInitializationListener(this));
+
         // Once the initialization callback is returned you can start loading your ads
         log("init ironSource SDK with appKey: " + APP_KEY);
-        IronSource.init(this, APP_KEY, new DemoInitializationListener(this));
-
-        // To initialize specific ad units:
-        // IronSource.init(this, APP_KEY, new InitializationListener(this), IronSource.AD_UNIT.REWARDED_VIDEO, IronSource.AD_UNIT.INTERSTITIAL, IronSource.AD_UNIT.BANNER);
 
         // Scroll down the file to find out what happens when you tap a button...
     }
@@ -119,51 +134,54 @@ public class DemoActivity extends Activity implements DemoActivityListener {
 
     public void loadInterstitialButtonTapped(View view) {
         // This will load an Interstitial ad
-
+        mInterstitialAd = new LevelPlayInterstitialAd(INTERSTITIAL_AD_UNIT_ID);
+        mInterstitialAd.setListener(new DemoInterstitialAdListener(this));
+        mInterstitialAd.loadAd();
         log("loadInterstitial");
-        IronSource.loadInterstitial();
     }
 
     public void showInterstitialButtonTapped(View view) {
         // It is advised to make sure there is available ad before attempting to show an ad
-        if (IronSource.isInterstitialReady()) {
+        if (mInterstitialAd.isAdReady()) {
             // This will present the Interstitial.
             // Unlike Rewarded Videos there are no placements.
 
             log("showInterstitial");
-            IronSource.showInterstitial();
+            mInterstitialAd.showAd(this);
         } else {
             // load a new ad before calling show
         }
     }
 
     public void loadBannerButtonTapped(View view) {
-        // call IronSource.destroyBanner() before loading a new banner
-        if (bannerParentLayout != null) {
-            this.destroyBanner();
+        // call destroy() before loading a new banner
+        if (mBannerAd != null) {
+            mBannerAd.destroy();
         }
 
         // choose banner size
-        // you can pick any banner size: ISBannerSize.BANNER, ISBannerSize.LARGE, ISBannerSize.RECTANGLE, ISBannerSize.SMART or even define a custom banner size by providing width and height
-        ISBannerSize size = ISBannerSize.BANNER;
+        // 1. recommended - Adaptive ad size that adjusts to the screen width
+        // 2. Adaptive ad size using fixed width ad size
+        // 3. Specific banner size - BANNER, LARGE, MEDIUM_RECTANGLE
+        LevelPlayAdSize adSize = LevelPlayAdSize.createAdaptiveAdSize(this);
 
-        // initialize IronSourceBanner object, using the IronSource.createBanner API
-        ironSourceBannerLayout = IronSource.createBanner(this, size);
+//        LevelPlayAdSize adSize = LevelPlayAdSize.createAdaptiveAdSize(this, 400);
+//        LevelPlayAdSize adSize = LevelPlayAdSize.BANNER;
 
-        if (ironSourceBannerLayout != null) {
-            // add IronSourceBanner to your container
-            FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT);
-            bannerParentLayout.addView(ironSourceBannerLayout, 0, layoutParams);
+        // Create the banner view and set the ad unit id and ad size
+        mBannerAd = new LevelPlayBannerAdView(this, BANNER_AD_UNIT_ID);
+        mBannerAd.setAdSize(adSize);
 
-            // set the banner listener
-            ironSourceBannerLayout.setLevelPlayBannerListener(new DemoBannerAdListener(this));
+        // add LevelPlayBannerAdView to your container
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT);
+        bannerParentLayout.addView(mBannerAd, 0, layoutParams);
 
-            // load ad into the created banner
-            log("loadBanner");
-            IronSource.loadBanner(ironSourceBannerLayout);
-        } else {
-            log("IronSource.createBanner returned null");
-        }
+        // set the banner listener
+        mBannerAd.setBannerListener(new DemoBannerAdListener(this));
+
+        // load ad into the created banner
+        log("loadBanner");
+        mBannerAd.loadAd();
     }
 
     public void destroyBannerButtonTapped(View view) {
@@ -173,8 +191,8 @@ public class DemoActivity extends Activity implements DemoActivityListener {
     private void destroyBanner() {
         if (bannerParentLayout != null) {
             log("destroyBanner");
-            IronSource.destroyBanner(ironSourceBannerLayout);
-            bannerParentLayout.removeView(ironSourceBannerLayout);
+            mBannerAd.destroy();
+            bannerParentLayout.removeView(mBannerAd);
             this.setBannerViewVisibility(View.GONE);
         }
 
